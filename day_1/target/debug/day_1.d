ec67b3d5916a78799c4644b939dbb5722fa1f085
Ben Crisman <benjamin.crisman@gmail.com>
2020-12-27 01:09:56 -0700

    nothing new

---
+++ day_1/target/debug/day_1.d
@@ -0,0 +1,1 @@
+/home/ben/projects/rust/advent_of_code_2020_rust/day_1/target/debug/day_1: /home/ben/projects/rust/advent_of_code_2020_rust/day_1/src/main.rs
---
+++ day_1/target/debug/deps/day_1-9cc9722b961134d6.d
@@ -0,0 +1,5 @@
+/home/ben/projects/rust/advent_of_code_2020_rust/day_1/target/debug/deps/day_1-9cc9722b961134d6: src/main.rs
+
+/home/ben/projects/rust/advent_of_code_2020_rust/day_1/target/debug/deps/day_1-9cc9722b961134d6.d: src/main.rs
+
+src/main.rs:
